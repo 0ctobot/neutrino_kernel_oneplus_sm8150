ddd9ba7e6c428bb71f7830cf564ee9b65e314b84
Park Ju Hyung <qkrwngud825@gmail.com>
2020-06-08 00:23:25 -0400

    kernfs: use buffer from the stack space
    Stack doesn't go all that deep when kernfs' fops is called.
    
    Use a big chunk of the stack space to avoid allocating heap memory repeatedly.
    
    Signed-off-by: Park Ju Hyung <qkrwngud825@gmail.com>
    [@0ctobot: This reverts 992500b / 284495d ("kernfs: Avoid
    dynamic memory allocation for small write buffers")]
    Signed-off-by: Adam W. Willis <return.of.octobot@gmail.com>

--- fs/kernfs/file.c
+++ fs/kernfs/file.c
@@ -284,8 +284,8 @@ static ssize_t kernfs_fop_write(struct file *file, const char __user *user_buf,
 {
 	struct kernfs_open_file *of = kernfs_of(file);
 	const struct kernfs_ops *ops;
-	char buf_onstack[SZ_4K + 1] __aligned(sizeof(long));
 	ssize_t len;
+	char stack_buf[PATH_MAX + 1];
 	char *buf;
 
 	if (of->atomic_write_len) {
@@ -297,16 +297,12 @@ static ssize_t kernfs_fop_write(struct file *file, const char __user *user_buf,
 	}
 
 	buf = of->prealloc_buf;
-	if (buf)
+	if (buf) {
 		mutex_lock(&of->prealloc_mutex);
-	else {
-		if (len < sizeof(buf_onstack)) {
-			buf = buf_onstack;
-		} else {
-			buf = kmalloc(len + 1, GFP_KERNEL);
-			if (!buf)
-				return -ENOMEM;
-		}
+		if (!buf)
+			return -ENOMEM;
+	} else {
+		buf = stack_buf;
 	}
 
 	if (copy_from_user(buf, user_buf, len)) {
@@ -341,8 +337,6 @@ static ssize_t kernfs_fop_write(struct file *file, const char __user *user_buf,
 out_free:
 	if (buf == of->prealloc_buf)
 		mutex_unlock(&of->prealloc_mutex);
-	else if (buf != buf_onstack)
-		kfree(buf);
 	return len;
 }
 
